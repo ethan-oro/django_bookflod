4f0d2d92f76f5884d70ca79dd05e7467a37fd0ee
Ethan Oro <ethanoro17@gmail.com>
2018-06-20 11:09:16 -0700

    bug with internal html links

--- bookflod/urls.py
+++ bookflod/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from bookflod.views import home, books, user_profiles
 
+app_name = "bookflod"
+
 urlpatterns = [
     path('', home),
-    path('books', books),
+    path('books', books, name = 'books'),
     path('userprofiles', user_profiles),
 ]
